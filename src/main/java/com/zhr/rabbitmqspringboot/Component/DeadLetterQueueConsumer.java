e5491fdc3b2320bc681353aa4fb63adf9404958f
zhanghaoran <2017929959@qq.com>
2022-11-24 12:43:59 +0800

    ""

--- src/main/java/com/zhr/rabbitmqspringboot/Component/DeadLetterQueueConsumer.java
+++ src/main/java/com/zhr/rabbitmqspringboot/Component/DeadLetterQueueConsumer.java
@@ -1,2 +1,20 @@
-package com.zhr.rabbitmqspringboot.Component;public class DeadLetterQueueConsumer {
+package com.zhr.rabbitmqspringboot.Component;
+
+import com.rabbitmq.client.Channel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Date;
+
+@Component
+@Slf4j
+public class DeadLetterQueueConsumer {
+    @RabbitListener(queues = "QD")
+    public void receiveD(Message message, Channel channel) throws IOException {
+        String msg = new String(message.getBody());
+        log.info("当前时间:{} ,收到死信队列消息{}",new Date(),msg);
+    }
 }
--- src/main/java/com/zhr/rabbitmqspringboot/Controller/SendMsgController.java
+++ src/main/java/com/zhr/rabbitmqspringboot/Controller/SendMsgController.java
@@ -1,2 +1,26 @@
-package com.zhr.rabbitmqspringboot.Controller;public class SendMsgController {
+package com.zhr.rabbitmqspringboot.Controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+@Slf4j
+@RequestMapping("ttl")
+public class SendMsgController {
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+    @GetMapping("sendMsg/{message}")
+    public void sendMsg(@PathVariable String message) {
+        log.info("当前时间:{},发送一条消息给两个ttl队列:{}",new Date(),message);
+        rabbitTemplate.convertAndSend("X","XA","消息来自ttl的为10s对的队列" + message);
+        rabbitTemplate.convertAndSend("X","XB","消息来自ttl的为40s对的队列" + message);
+    }
+
 }
